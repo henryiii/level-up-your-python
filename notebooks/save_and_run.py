fcc67286d28299652b6df47a5fb2c6dd5b2be9b4
Henry Schreiner <henryschreineriii@gmail.com>
2022-01-07 12:53:19 -0500

    feat: include extension

--- notebooks/save_and_run.py
+++ notebooks/save_and_run.py
@@ -2,6 +2,7 @@ from IPython.core.magic import Magics, magics_class, cell_magic
 
 import sys
 import subprocess
+from pathlib import Path
 
 
 @magics_class
@@ -9,19 +10,24 @@ class AutoMagics(Magics):
     @cell_magic
     def save_and_run(self, line, cell, local_ns=None):
         commands = line.split()
-        filename = "tmp.py"
-        with open(filename, "w") as f:
-            f.write(cell)
+        filepath = Path("tmp.py")
+        filepath.write_text(cell)
 
         subprocess.run(
             [sys.executable, "-m", *commands, filename],
+            # This is just to support colors in the notebook
             env={"FORCE_COLOR": "1", "MYPY_FORCE_COLOR": "1", "TERM": "xterm-color"},
         )
 
 
 # Why sys.executable here?
 #
-# We are running inside a virtual environment, which also has mypy installed. However, this is a shell command (the starting `!`), so it will not  necessarily run in the same environment. So we'll use the current python interpreter `sys.executable` and use the `python -m mypy` expression  instead of plain `mypy` to make sure we get our installed MyPy. You don't usually run mypy from a notebook.
+# We are running inside a virtual environment, which also has mypy installed.
+# However, this is a shell command (the starting `!`), so it will not
+# necessarily run in the same environment. So we'll use the current python
+# interpreter `sys.executable` and use the `python -m mypy` expression  instead
+# of plain `mypy` to make sure we get our installed MyPy. You don't usually run
+# mypy from a notebook.
 
 
 def load_ipython_extension(ipython):
