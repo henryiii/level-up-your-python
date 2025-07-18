778fad0c2796d352c552b35cddab75bf25525814
Henry Schreiner <henryschreineriii@gmail.com>
2025-07-17 20:43:57 -0700

    chore: switch to pixi
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -5,9 +5,15 @@ nox.needs_version = ">=2022.1.7"
 
 @nox.session(reuse_venv=True)
 def pyodide(session: nox.Session) -> None:
-    session.install("jupyterlite[lab]")
+    session.install(
+        "jupyterlite-core~=0.6.2",
+        "jupyterlab~=4.4.3",
+        "notebook~=7.4.3",
+        "jupyterlite-pyodide-kernel~=0.6.1",
+    )
     session.run("jupyter", "lite", "init")
-    session.run("jupyter", "lite", "build", "--contents=notebooks")
+    session.run("jupyter", "lite", "build", "--contents=content")
 
-    if "--serve" in session.posargs:
+    if session.interactive:
         session.run("jupyter", "lite", "serve")
+
