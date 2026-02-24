7a375c49df9eef3cca77191956624c197a587ea3
Henry Schreiner <henryfs@princeton.edu>
2026-02-24 17:27:17 -0500

    chore: update version numbers
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- noxfile.py
+++ noxfile.py
@@ -6,14 +6,13 @@ nox.needs_version = ">=2022.1.7"
 @nox.session(reuse_venv=True)
 def pyodide(session: nox.Session) -> None:
     session.install(
-        "jupyterlite-core~=0.6.2",
-        "jupyterlab~=4.4.3",
-        "notebook~=7.4.3",
-        "jupyterlite-pyodide-kernel~=0.6.1",
+        "jupyterlite-core~=0.7.2",
+        "jupyterlab~=4.5.5",
+        "notebook~=7.5.4",
+        "jupyterlite-pyodide-kernel~=0.7.0",
     )
     session.run("jupyter", "lite", "init")
     session.run("jupyter", "lite", "build", "--contents=notebooks")
 
     if session.interactive:
         session.run("jupyter", "lite", "serve")
-
